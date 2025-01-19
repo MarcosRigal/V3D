6fc20cf90db0397fbfde25d9ddb05f4ff6f0567f
MarcosRigal <riveragavilanmarcos@gmail.com>
2025-01-19 18:06:10 +0100

    Done P8

--- P8/stereo_checkundistorted.cpp
+++ P8/stereo_checkundistorted.cpp
@@ -1,35 +1,29 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-// Stereo parameters structure
 struct StereoParams {
     cv::Mat mtxL, distL, R_L, T_L;
     cv::Mat mtxR, distR, R_R, T_R;
     cv::Mat Rot, Trns, Emat, Fmat;
 };
 
-// Global variables for mouse callback
 cv::Mat originalDisplay, rectifiedDisplay;
 int mouseY = -1;
 
-// Mouse callback for drawing dynamic horizontal line
 void onMouse(int event, int x, int y, int flags, void* userdata) {
     if (event == cv::EVENT_MOUSEMOVE) {
         mouseY = y;
 
-        // Redraw Original Images with Line
         cv::Mat originalCopy = originalDisplay.clone();
-        cv::line(originalCopy, cv::Point(0, mouseY), cv::Point(originalCopy.cols, mouseY), cv::Scalar(0, 255, 0), 1);
+        cv::line(originalCopy, cv::Point(0, mouseY), cv::Point(originalCopy.cols, mouseY), cv::Scalar(0, 0, 255), 1);
         cv::imshow("Original Images", originalCopy);
 
-        // Redraw Rectified Images with Line
         cv::Mat rectifiedCopy = rectifiedDisplay.clone();
-        cv::line(rectifiedCopy, cv::Point(0, mouseY), cv::Point(rectifiedCopy.cols, mouseY), cv::Scalar(0, 255, 0), 1);
+        cv::line(rectifiedCopy, cv::Point(0, mouseY), cv::Point(rectifiedCopy.cols, mouseY), cv::Scalar(0, 0, 255), 1);
         cv::imshow("Rectified Images", rectifiedCopy);
     }
 }
 
-// Load stereo calibration parameters
 bool loadStereoCalibration(const std::string &filename, StereoParams &params) {
     cv::FileStorage fs(filename, cv::FileStorage::READ);
     if (!fs.isOpened()) {
@@ -85,35 +79,28 @@ int main(int argc, char **argv) {
     std::string stereoImagePath = argv[1];
     std::string calibrationFile = argv[2];
 
-    // Load stereo image
     cv::Mat stereoImg = cv::imread(stereoImagePath);
     if (stereoImg.empty()) {
         std::cerr << "❌ Error loading stereo image.\n";
         return -1;
     }
 
-    // Split stereo image into left and right
     cv::Mat leftImg = stereoImg(cv::Rect(0, 0, stereoImg.cols / 2, stereoImg.rows)).clone();
     cv::Mat rightImg = stereoImg(cv::Rect(stereoImg.cols / 2, 0, stereoImg.cols / 2, stereoImg.rows)).clone();
 
-    // Load stereo calibration parameters
     StereoParams params;
     if (!loadStereoCalibration(calibrationFile, params)) {
         return -1;
     }
 
-    // Display original images
     cv::hconcat(leftImg, rightImg, originalDisplay);
     cv::imshow("Original Images", originalDisplay);
 
-    // Rectify images
     rectifyStereoImages(params, leftImg, rightImg);
 
-    // Display rectified images
     cv::hconcat(leftImg, rightImg, rectifiedDisplay);
     cv::imshow("Rectified Images", rectifiedDisplay);
 
-    // Set mouse callback
     cv::setMouseCallback("Original Images", onMouse, nullptr);
     cv::setMouseCallback("Rectified Images", onMouse, nullptr);
 
